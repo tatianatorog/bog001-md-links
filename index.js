95ddf44db5b8cf7052912cde40b5ee1a62d86926
TATIANA Toro <tatianatorog@gmail.com>
2020-09-25 16:44:20 -0500

    resolviendo promesas

--- index.js
+++ index.js
@@ -5,56 +5,7 @@
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
-const glob = require("glob")
-
-// const getDirectories = function (src, callback) {
-//   glob(src + '**/*.md', callback);
-// };
-// getDirectories('/Users/albalucia/Desktop/curso/', function (err, res) {
-//   if (err) {
-//     console.log('Error', err);
-//   } else {
-//     console.log(res);
-//   }
-// });
-
-function walk(directory, filepaths = []) {
-  const files = fs.readdirSync(directory);
-  console.log(files)
-  for (let filename of files) {
-      const filepath = path.join(directory, filename);
-      console.log(filepath)
-
-      if (fs.statSync(filepath).isDirectory()) {
-          walk(filepath, filepaths);
-      } else if (path.extname(filename) === '.md') {
-          filepaths.push(filepath);
-      }
-  }
-  // return filepaths;
-}
-
-
-
-
-
-// const crawl = (dir) => {
-//   console.log('[+]', dir);
-//   let files = fs.readdirSync(dir);
-//   for (let x in files) {
-//     let next = path.join(dir, files[x]);
-
-//     if (fs.lstatSync(next).isDirectory() == true) {
-//       crawl(next);
-//     }
-//     else {
-//       console.log(next);
-//     }
-//   }
-// }
-
-console.log(walk("/Users/albalucia/Desktop/curso/"))
-
+const pathToCheck = "/Users/albalucia/Desktop/curso/modulos/prueba.md"
 
 
 // AXIOS Realizar peticiones HTTP desde Nodejs.
@@ -65,31 +16,29 @@ const findLinksInMd = (path) => {
   return new Promise((resolve, reject) => {
     fs.readFile(path, "utf-8", function (err, data) {
       const regex = /\[([^\[]+)\](\(.*\))/gm;
-      const array = data.match(regex);
+      const arrayOfLinks = data.match(regex);
       if (err) {
         console.log('No found')
         reject(err)
       }
-      if (!array) {
+      if (!arrayOfLinks) {
         console.log("No links found it")
-         reject(err)
+        reject(err)
       } else {
-        const links = array.map((item) => {
+        const links = arrayOfLinks.map((item) => {
           const textHrefDivide = item.split("](")
           const text = textHrefDivide[0].replace("[", "");
           const href = textHrefDivide[1].replace(")", "");
           return ({ href, text, path });
         });
-        console.log(links)
+        resolve(links)
       }
     });
   });
 };
 
 
-findLinksInMd("/Users/albalucia/Desktop/curso/modulos/prueba.md")
-.then((links)=> console.log(links))
-.catch(error => console.error(error));
+
 
 
 
@@ -103,19 +52,20 @@ const linkValidate = (url, text, path) =>
       )
       .catch(() => resolve({ url: url, text: text, file: path, status: 404, statusText: 'FAIL' }))
   );
+
 const linksValidatePromises = [];
 
 const resolveValidate = (links) => {
-  links.forEach(({ href, text, path }) =>
+  links.map(({ href, text, path }) =>
     linksValidatePromises.push(linkValidate(href, text, path))
   );
   Promise.all(linksValidatePromises)
     .then((stats) => {
-      // resolve(stats)
+      // return (stats)
       console.log(stats)
         ;
     })
-    .catch(() => reject(new Error(`no links to validate were found on the ${path}`)));
+    .catch(() => reject(new Error(`No links to validate were found on the ${path}`)));
 };
 
 
@@ -146,29 +96,52 @@ const linksStats = (links) => {
 };
 // The split method divides a String into an ordered list of substrings into an ordered list of substrings and returns an array.
 
-// console.log(linksStats([
-//   {
-//     url: 'https://es.wikipedia.org/wiki/Markdown',
-//     text: 'Markdown',
-//     file: '/Users/albalucia/Desktop/bog001-md-links/README.md',
-//     status: 200,
-//     statusText: 'OK'
-//   },
-//   {
-//     url: 'https://nodejs.org/',
-//     text: 'Node.js',
-//     file: '/Users/albalucia/Desktop/bog001-md-links/README.md',
-//     status: 200,
-//     statusText: 'OK'
-//   },
-//   {
-//     url: 'https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg',
-//     text: 'md-links',
-//     file: '/Users/albalucia/Desktop/bog001-md-links/README.md',
-//     status: 200,
-//     statusText: 'OK'
-//   }
-// ]))
+console.log(linksStats([
+  {
+    url: 'https://es.wikipedia.org/wiki/Markdown',
+    text: 'Markdown',
+    file: '/Users/albalucia/Desktop/bog001-md-links/README.md',
+    status: 404,
+    statusText: 'OK'
+  },
+  {
+    url: 'https://nodejs.org/',
+    text: 'Node.js',
+    file: '/Users/albalucia/Desktop/bog001-md-links/README.md',
+    status: 404,
+    statusText: 'OK'
+  },
+  {
+    url: 'https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg',
+    text: 'md-links',
+    file: '/Users/albalucia/Desktop/bog001-md-links/README.md',
+    status: 200,
+    statusText: 'OK'
+  }
+]))
 
 
 
+const arrayOfPaths = ['/Users/albalucia/Desktop/bog001-md-links/readmePrueba copy.md', "/Users/albalucia/Desktop/bog001-md-links/readmePrueba.md"]
+const filesPromises = [];
+
+const getLinksOfFiles = (pathsMdFiles) => {
+pathsMdFiles
+  .reverse()
+  .forEach((file) => filesPromises.push(findLinksInMd(file)));
+};
+// console.log(getLinksOfFiles(arrayOfPaths))
+// console.log(getLinksOfFiles(arrayOfPaths))
+
+// })
+findLinksInMd(pathToCheck)
+.then((links)=> {
+  return resolveValidate(links)
+})
+.then((stats)=> {
+  // console.log(stats)
+  return resolveValidate(stats)})
+
+
+  // .then((links)=> {return resolveValidate(links) })
+// .catch(error => console.error(error));
