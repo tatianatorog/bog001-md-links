e8c9008293a986c012d7071749c60c41cef4b411
TATIANA Toro <tatianatorog@gmail.com>
2020-09-30 18:27:05 -0500

    Initializing tes

--- readmePrueba copy.md
+++
@@ -1,6 +0,0 @@
-Estos archivos `Markdown` normalmente contienen _links_ (vínculos/ligas) que
-muchas veces están rotos o ya no son válidos y eso perjudica mucho el valor de
-la información que se quiere compartir.
-
-Dentro de una comunidad de código abierto, nos han propuesto crear una
-herramienta usando
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,17 +1,37 @@
-const mock = require('/Users/albalucia/Desktop/bog001-md-links/test/mock.js');
+// const mock = require('/Users/albalucia/Desktop/bog001-md-links/test/mock.js');
 const path = require('path');
-const findLinksInMd = require('/Users/albalucia/Desktop/bog001-md-links/src/readFile.js');
+const arrayMock = require('./mock.js');
+// const findLinksInMd = require('../src/functions/obtainLinks.js');
+const utils = require('../src/functions/obtainLinks.js')
 
+describe('getLinks', () => {
 
-
-test('Retorna los personajes de Rick & Morty', () => {
-  return expect(findLinksInMd("readmePrueba.md")).resolves.toHaveProperty('href');
+  it('Should be a function', () => {
+    expect(typeof utils.getLinksInMd).toBe('function');
+  });
 });
 
-// test.only('Falla cuando la url está mal escrita', () => {
-//   const myerror = new Error('Network Error');
-//   return expect(getCharacters('holi')).rejects.toEqual(myerror);
-// });
+it('Should be an array of objects', () => {
+  const userPath = 'test/readmePrueba.md';
+  return utils.getLinksInMd(userPath).then(links => {
+      expect(links).toEqual(arrayMock)
+  })
+});
+
+it('Fails when there are not links in file', () => {
+  const userPath = 'test/readmeNoLinks.md';
+  return utils.getLinksInMd(userPath).catch(e => {
+      expect(e.message).toBe('No links found it')
+  });
+});
+
+it('Fails when the path is not correct', () => {
+  const userPath = 'course/readmeNoLinks.md';
+  return utils.getLinksInMd(userPath).catch(e => {
+      expect(e.message).toBe("No such a file")
+  });
+});
+
 
 
 
